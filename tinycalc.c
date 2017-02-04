12c2d2a1912cd98751b2e10d9db54316fbf471b6
Samadi van Koten <me@vktec.co.uk>
2017-02-04 22:31:57 +0000

    Increase precision of output to 20 sig. fig.

--- tinycalc.c
+++ tinycalc.c
@@ -14,7 +14,7 @@ int main() {
     result = te_interp(expr, &error);
 
     if (error == 0) {
-      printf("%g\n", result);
+      printf("%.20g\n", result);
     } else {
       printf("Parse error:\n  %s\n  ", expr);
       for (int i = 1; i < error; i++) {
