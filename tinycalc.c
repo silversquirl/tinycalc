9bc74cf629cd594734f86a7be974a3e1e6604561
Samadi van Koten <samadi@vktec.co.uk>
2018-04-30 20:25:34 +0100

    Switch to tabs for indentation

--- tinycalc.c
+++ tinycalc.c
@@ -3,17 +3,17 @@
 #include "tinyexpr/tinyexpr.h"
 
 int main() {
-  char *expr;
-  int error;
+	char *expr;
+	int error;
 
-  while ((expr = linenoise("> "))) {
-    linenoiseHistoryAdd(expr);
-    double result = te_interp(expr, &error);
-    if (error == 0)
-      printf("%.20g\n", result);
-    else
-      printf("Parse error:\n  %s\n  %*.1s\n", expr, error, "^");
-    linenoiseFree(expr);
-  }
-  return 0;
+	while ((expr = linenoise("> "))) {
+		linenoiseHistoryAdd(expr);
+		double result = te_interp(expr, &error);
+		if (error == 0)
+			printf("%.20g\n", result);
+		else
+			printf("Parse error:\n  %s\n  %*.1s\n", expr, error, "^");
+		linenoiseFree(expr);
+	}
+	return 0;
 }
